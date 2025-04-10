7509866be042176caa950ee20e944163c76e14e3
Vasanth kumar AM <vasanthkumar17112002@gmail.com>
2025-04-10 15:01:50 +0530

    performed build

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+    return (
+      <div className="text-center mt-10 text-xl text-blue-600">
+        🚀 Welcome to Devora Landing Page!
+      </div>
+    );
+  }
+  
+  export default App;
+  
