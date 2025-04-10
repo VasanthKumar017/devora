0e6e93d1dc5250eb9d1d51d0bd307858c7947df1
Vasanth kumar AM <vasanthkumar17112002@gmail.com>
2025-04-10 15:59:46 +0530

    Added route for navigation

---
+++ client/src/routes/router.jsx
@@ -0,0 +1,23 @@
+// src/router.jsx
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
+import Header from "./components/Header/Header";
+
+const Router = () => (
+  <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default Router;
